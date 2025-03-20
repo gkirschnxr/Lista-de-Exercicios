3c10b54f0b84e6f4b1fc3d3ffba1b3f20af62a54
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-20 15:44:39 -0300

    Resolução exercicio 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio02
+{
+    // Crie um programa para calcular o volume de um Cilindro
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // V = πr²h
+            Console.Write("Digite o valor do raio em metros: ");
+            // raio
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura em metros: ");
+            // altura
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double V = Math.PI * Math.Pow(r, 2) * h;
+
+            Console.WriteLine($"O volume do cilindro é: {V} m³");
+            Console.ReadLine();
+        }
+    }
+}
