4b153dadbbe8225f4c6d58e13721f0941c2ce79f
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-24 13:31:58 -0300

    Resolução exercício 18

---
+++ Exercício18/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercício18;
+
+// Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 100; i <= 200; i++)
+        {
+            if (i % 2 != 0)
+               Console.WriteLine($"{i}");
+        }
+
+        Console.ReadLine();
+    }
+}
