4a9fe29e34d188af14357ace8b1b71c0ff37b37c
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-24 13:14:42 -0300

    Resolução exercício 16

---
+++ Exercício16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercício16
+{
+    // Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.WriteLine("O número digitado é par.");
+
+            else
+                Console.WriteLine("O número digitado é ímpar.");
+
+            Console.ReadLine();
+        }
+    }
+}
