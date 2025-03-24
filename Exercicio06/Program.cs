9755b9c82f46e84d2b45ce19fda6759c6d4bdd2c
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-24 17:04:09 -0300

    Resolução exercicio 6

--- Exercicio06/Program.cs
+++ Exercicio06/Program.cs
@@ -1,43 +1,29 @@
 ﻿using System.Numerics;
 
-namespace Exercicio06
+namespace Exercicio06;
+
+//      Crie um programa para calcular a média harmônica das notas de um Aluno
+internal class Program
 {
-    //      Crie um programa para calcular a média harmônica das notas de um Aluno
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Digite suas ultimas 3 notas para descobrir a média harmonica entre elas: ");
+        Console.WriteLine("Digite suas ultimas 3 notas para descobrir a média harmonica entre elas: ");
 
-            Console.WriteLine("Primeira nota: ");
-            decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Segunda nota: ");
-            decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Terceira nota: ");
-            decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Terceira nota: ");
+        double nota3 = Convert.ToDouble(Console.ReadLine());
 
-            decimal notasTotais = 3;
-
-            decimal calculo2 = 1 / nota1 + 1 / nota2 + 1 / nota3;
-
-            decimal calculo1 = notasTotais / calculo2;
-            
-            decimal calculo3 = Math.Abs(nota1 * nota2);
-
-            int mdc(int a, int b) 
-            { 
-                if (a == 0) return b; return mdc(b % a, a); 
-            
-            
-            
-            }
+        double resultado = 3.0 / ((1.0 / nota1) + (1.0 / nota2) + (1.0 / nota3));
 
 
+        Console.WriteLine("A média harmonica do aluno é: " + Math.Round(resultado));
 
-            Console.WriteLine($"media {calculo3}");
-            Console.ReadLine();
-        }
+        Console.WriteLine();
+        Console.ReadLine();
     }
 }
