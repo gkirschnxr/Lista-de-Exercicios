cd709ae2c650967c7734aaa974aa128f3c88e1d6
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-24 09:18:36 -0300

    Resolução exercicio07

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace Exercicio06
+{
+    //      Crie um programa para calcular a média harmônica das notas de um Aluno
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite suas ultimas 3 notas para descobrir a média harmonica entre elas: ");
+
+            Console.WriteLine("Primeira nota: ");
+            decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Segunda nota: ");
+            decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Terceira nota: ");
+            decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal notasTotais = 3;
+
+            decimal calculo2 = 1 / nota1 + 1 / nota2 + 1 / nota3;
+
+            decimal calculo1 = notasTotais / calculo2;
+            
+            decimal calculo3 = Math.Abs(nota1 * nota2);
+
+            int mdc(int a, int b) 
+            { 
+                if (a == 0) return b; return mdc(b % a, a); 
+            
+            
+            
+            }
+
+
+
+            Console.WriteLine($"media {calculo3}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio07
+{
+    // Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua primeira nota: ");
+            decimal primeiraNota = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite sua segunda nota: ");
+            decimal segundaNota = Convert.ToDecimal(Console.ReadLine());
+
+            decimal totalDeNotas = 2;
+
+            decimal resultado = (primeiraNota + segundaNota) / totalDeNotas;
+
+
+            Console.WriteLine("A média ponderada das suas notas é: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
