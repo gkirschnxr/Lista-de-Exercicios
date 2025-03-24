a9836e614e28b513b6eee66fd3c6d996da5ffe3e
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-24 13:49:43 -0300

    Resolução exercício 19

---
+++ Exercício19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercício19
+{
+    // Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se
+    // encontram no conjunto dos números de 1 até 500.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 500; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
